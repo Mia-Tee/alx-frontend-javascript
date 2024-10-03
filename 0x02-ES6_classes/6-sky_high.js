1afbee1196396c610ec1097bb75a2c0e611e3b8f
Mia-Tee <tanyatsekenie@gmail.com>
2024-10-03 16:11:16 +0000

    Inheritance

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,10 +1,18 @@
-export default class Airport {
-  constructor(name, code) {
-    this._name = name;
-    this._code = code;
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
   }
 
-  get [Symbol.toStringTag]() {
-    return `${this._code}`;
+  // Getter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  // override method
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
